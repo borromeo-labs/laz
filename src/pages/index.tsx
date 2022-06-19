15d9c2a8f260978df68f6080ff85ea46cc965165
srph <seraphipod@gmail.com>
2022-06-20 02:19:55 +0800

    Sample

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div>Hello</div>
+  return (
+    <div>
+      <div className="flex items-center h-[64px] w-[64px] text-neutral-100 bg-neutral-900">La-z</div>
+    </div>
+  )
 }
