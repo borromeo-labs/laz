23c63398fcbcb32907fe062b89ed0dae8da5e0d7
srph <seraphipod@gmail.com>
2022-06-28 05:14:58 +0800

    More updates

--- api/app/Models/ExpenseGroup.php
+++ api/app/Models/ExpenseGroup.php
@@ -10,6 +10,16 @@ class ExpenseGroup extends Model
 {
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'amount_total'
+    ];
+
     public function user() {
         return $this->belongsTo(User::class)
     }
