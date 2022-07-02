937f7eeb9c779c135b875dd16cd80c5f72875f5a
srph <seraphipod@gmail.com>
2022-07-02 14:05:11 +0800

    Move default font size on body

--- ui/src/pages/_app.tsx
+++ ui/src/pages/_app.tsx
@@ -1,11 +1,7 @@
 import '@/styles/globals.css'
 
 function App({ Component, pageProps }) {
-  return (
-    <div className="text-h5">
-      <Component {...pageProps} />
-    </div>
-  )
+  return <Component {...pageProps} />
 }
 
 export default App
