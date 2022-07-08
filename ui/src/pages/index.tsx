ffe39f23fa55b4fc3609d8af1bd9d69e4ab74b95
Kirby Borromeo <kerdaddyb@gmail.com>
2022-07-08 10:18:44 +0800

    React Icons installed. Changed the svg icons to react icons.

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -1,4 +1,7 @@
 import { ExpenseClerk } from '@/page-components/ExpenseClerk'
+import { IoChevronForwardOutline } from 'react-icons/io5'
+import { IoChevronBackOutline } from 'react-icons/io5'
+import { IoCaretDownOutline } from 'react-icons/io5'
 
 export default function Home() {
   return (
@@ -11,38 +14,17 @@ export default function Home() {
 
             <div className="flex justify-center w-half">
               <button className="px-12 py-8 mr-8 rounded hover:bg-slate-200 duration-100 ease-linear">
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    d="M15.375 5.25L8.625 12L15.375 18.75"
-                    stroke="#2C2B3C"
-                    stroke-width="2.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
+                <IoChevronBackOutline  />
               </button>
 
               <button className="px-12 py-8 mr-12 rounded hover:bg-slate-200 duration-100 ease-linear">
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    d="M8.625 5.25L15.375 12L8.625 18.75"
-                    stroke="#2C2B3C"
-                    stroke-width="2.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
+                <IoChevronForwardOutline  />
               </button>
 
               <button className="flex items-center px-16 py-8 rounded hover:bg-slate-200 duration-100 ease-linear">
                 <p className="font-sans text-xl mr-8">June 2022</p>
                 <i>
-                  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      d="M3.0625 5.93942L7.43062 11.0369C7.50103 11.119 7.58837 11.185 7.68665 11.2302C7.78493 11.2753 7.89183 11.2987 8 11.2987C8.10817 11.2987 8.21507 11.2753 8.31334 11.2302C8.41162 11.185 8.49896 11.119 8.56937 11.0369L12.9375 5.93942C13.3544 5.45286 13.0087 4.70129 12.3681 4.70129H3.63062C2.99 4.70129 2.64437 5.45286 3.0625 5.93942Z"
-                      fill="#2C2B3C"
-                    />
-                  </svg>
+                  <IoCaretDownOutline />
                 </i>
               </button>
             </div>
