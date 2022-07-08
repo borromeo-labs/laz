1b0ac35ceed62c9c64af14acb16b9d73a65d1ef2
Kirby Borromeo <kerdaddyb@gmail.com>
2022-07-08 09:54:13 +0800

    Expenses table

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -3,7 +3,8 @@ import { ExpenseClerk } from '@/page-components/ExpenseClerk'
 export default function Home() {
   return (
     <div>
-      <div className="flex-col bg-[#FAFBFF]">
+      <div className="flex-col bg-[#FAFBFF] h-screen">
+        {/* Navbar */}
         <div className="bg-white border-b-[1px] border-slate-200">
           <div className="flex justify-between max-w-[1440px] mx-auto px-56 py-12">
             <div className="w-1/4"></div>
@@ -55,17 +56,25 @@ export default function Home() {
             </div>
           </div>
         </div>
+        {/* Table Component */}
         <div className="mx-auto py-[56px] max-w-[560px]">
-          <div className="bg-white height-full border-[1px] rounded-lg">
+          <div className="bg-white height-full rounded-lg outline outline-1 outline-slate-200">
+            {/* Panel Header */}
             <div className="p-16 border-b-[1px]">
               <p className="text-[12.8px] font-semibold ">Total Summary</p>
               <p className="">₱3,000.00</p>
             </div>
+            {/* Table Heading */}
             <div className="flex">
               <div className="shrink-0 w-[160px] bg-slate-300 px-16 py-12 text-[12.8px] font-semibold border-r-[1px]">
                 Amount
               </div>
               <div className="bg-slate-300 px-16 py-12 text-[12.8px] font-semibold w-full">Description</div>
+            </div>
+            {/* Table Items */}
+            <div className="flex">
+              <input type="number" className="shrink-0 w-[160px] px-16 py-12 border-r-[1px] focus:outline-none focus:ring-2 focus:ring-blue-500 ring-inset"></input>
+              <input className="w-full px-16 py-12 border-r-[1px] focus:outline-none focus:ring-2 focus:ring-blue-500 ring-inset"></input>
             </div>
           </div>
         </div>
