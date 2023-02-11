274d73a7ca35a9056f667243314a6ecb96f3eafe
Kier Borromeo <seraphipod@gmail.com>
2023-02-11 19:35:33 +0800

    Add mysql to sail;

--- ui/src/page-components/ExpenseMonth/mutations/useDeleteExpenseMonthMutation.ts
+++ ui/src/page-components/ExpenseMonth/mutations/useDeleteExpenseMonthMutation.ts
@@ -1,15 +1,26 @@
 import { useAxios } from '@/contexts/Axios'
 import { useMutation } from 'react-query'
 import { ExpenseItem } from '@/types/api'
+import { useExpenseMonth } from '../ExpenseMonthProvider'
 
 const useDeleteExpenseMonthMutation = () => {
   const { axios } = useAxios()
+
+  const { deleteItem } = useExpenseMonth()
 
   const handleRequest = (item: ExpenseItem) => {
     return axios.delete(`expense-items/${item.id}`)
   }
 
-  return useMutation(handleRequest)
+  // Add error-handling; undo delete and bring back item when error occurs
+  const { mutate: mutationFn, ...mutationData } = useMutation(handleRequest)
+
+  const mutate = (item: ExpenseItem) => {
+    deleteItem(item)
+    mutationFn(item)
+  }
+
+  return { mutate, ...mutationData }
 }
 
 export { useDeleteExpenseMonthMutation }
