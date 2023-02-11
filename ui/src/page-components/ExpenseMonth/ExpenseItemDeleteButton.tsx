f4bd1b95fb52c0074475e9a4cafad649c0669e82
Kier Borromeo <seraphipod@gmail.com>
2023-02-12 00:10:52 +0800

    WIP: Hold to delete

--- ui/src/page-components/ExpenseMonth/ExpenseItemDeleteButton.tsx
+++ ui/src/page-components/ExpenseMonth/ExpenseItemDeleteButton.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import cx from 'classnames'
+import { motion } from 'framer-motion'
 import { Button } from '@/components'
 import { Popover } from '@headlessui/react'
 import { IoArchiveOutline } from 'react-icons/io5'
@@ -13,6 +14,10 @@ interface ExpenseItemDeleteButtonProps {
 const ExpenseItemDeleteButton: React.FC<ExpenseItemDeleteButtonProps> = ({ item }) => {
   const { mutate, isLoading } = useDeleteExpenseMonthMutation()
 
+  // Framer motion hold click
+
+  const [isHovered, setIsHovered] = useState(false)
+
   return (
     <div className="absolute top-0 bottom-0 right-0 flex translate-x-full items-center px-8">
       <div className="-translate-x-4 opacity-0 transition-transform group-focus-within:translate-x-0 group-focus-within:opacity-100 group-hover:translate-x-0 group-hover:opacity-100">
@@ -25,6 +30,32 @@ const ExpenseItemDeleteButton: React.FC<ExpenseItemDeleteButtonProps> = ({ item 
 
           <Popover.Panel className="z-10 absolute top-full w-[240px]  rounded border border-neutral-200 bg-white p-16">
             <p>Are you sure to do this?</p>
+
+            <div className="h-8" />
+
+            <motion.button
+              whileHover="hover"
+              onHoverStart={() => setIsHovered(true)}
+              onHoverEnd={() => setIsHovered(false)}
+              className="relative block w-full rounded bg-neutral-200 py-12 px-12">
+              <span className="z-10 relative flex items-center leading-none text-neutral-600">
+                <IoArchiveOutline />
+                <span className="ml-8">{isHovered ? 'Hold to confirm' : 'Delete'}</span>
+              </span>
+
+              <div className="z-20 absolute top-0 right-0 bottom-0 left-0">
+                <motion.div
+                  initial={{ width: '0%' }}
+                  variants={{ hover: { width: '100%' } }}
+                  transition={{ duration: 1 }}
+                  className="h-full overflow-hidden rounded bg-red-200">
+                  <span className="flex h-full w-[206px] items-center py-12 px-12 leading-none text-red-500">
+                    <IoArchiveOutline />
+                    <span className="ml-8">Hold to confirm</span>
+                  </span>
+                </motion.div>
+              </div>
+            </motion.button>
           </Popover.Panel>
         </Popover>
       </div>
