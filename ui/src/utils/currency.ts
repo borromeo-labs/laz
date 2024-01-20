24f3b1409b17ebcf43f4c2dd80525fda71cedaa2
Kier Borromeo <seraphipod@gmail.com>
2024-01-20 23:31:01 +0800

    Prevent long numerical numbers from overflowing modal

--- ui/src/utils/currency.ts
+++ ui/src/utils/currency.ts
@@ -1,5 +1,15 @@
 const formatCurrency = (number) => {
-  return Number(number).toLocaleString('en-US', { style: 'currency', currency: 'USD' }).replace('$', '₱')
+  const value = Number(number)
+
+  // Prevent long numerical values from overflowing the modal at least.
+  // Feeling very grateful that we somehow need to handle this case :)
+  // 1,444,332 -> 1.44m
+  return value >= 1e6 ? `${format(value / 1e6)}m` : format(value)
+}
+
+const format = (value: number) => {
+  // 1324.324 -> 1,324.32
+  return value.toLocaleString('en-US', { style: 'currency', currency: 'USD' }).replace('$', '₱')
 }
 
 export { formatCurrency }
